833f9ecf958394ced7bb1e284f6534e0281f5d03
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2018-09-27 22:36:09 +0530

    Added first templates to the App

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This is the index page of the polls application of the website")
+    questions = Question.objects.order_by('pub_date')[:5]
+    context = {
+        'questions': questions
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request,question_id):
-    return HttpResponse('This is the detail of question_id {ques_id}'.format(ques_id = question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
