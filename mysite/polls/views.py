23f0f6e727ae79988e998fd8050e3b510dc0a66e
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2018-09-29 23:58:12 +0530

    Added first form in the Project

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
+from django.urls import reverse
 
 # Create your views here.
 
@@ -26,4 +27,18 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse('This is the vote page for page question_id {ques_id}'.format(ques_id = question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
