3f8d8c227d27932ef562bbdc1fe255ceb59d5e0f
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2018-10-09 11:29:05 +0530

    Completed the Course

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 
@@ -23,7 +23,8 @@ def detail(request,question_id):
 
 
 def results(request, question_id):
-    return HttpResponse('This is the results page of question_id {ques_id}'.format(ques_id = question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
